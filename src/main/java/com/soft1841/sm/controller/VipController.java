a84a4f4ee476c4af3bdb8ceba05d0712a6bce8c6
侯粤嘉 <2298090131@qq.com>
2018-12-24 22:32:20 +0800

    我做了会员管理模块中的会员信息展示，有删除功能和新增功能。

---
+++ src/main/java/com/soft1841/sm/controller/VipController.java
@@ -0,0 +1,173 @@
+package com.soft1841.sm.controller;
+
+import cn.hutool.db.Entity;
+import com.soft1841.sm.dao.VipDAO;
+import com.soft1841.sm.entity.Vip;
+import com.soft1841.sm.utils.DAOFactory;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+public class VipController implements Initializable {
+    @FXML
+    private FlowPane vipPane;
+    private VipDAO vipDAO = DAOFactory.getVipDAOInstance();
+    List<Entity> vipList = new ArrayList<>();
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            vipList = vipDAO.selectVip();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        //读取数据库信息
+        showVip(vipList);
+    }
+
+    private void showVip(List<Entity> vipList) {
+
+        //通过循环遍历readerList集合，创建Hbox来显示每个读者信息
+        //清除之前内容
+        vipPane.getChildren().clear();
+        //移除之前的记录
+        ObservableList<Node> observableList = vipPane.getChildren();
+        vipPane.getChildren().remove(observableList);
+
+        for (Entity entity:vipList) {
+            HBox hBox = new HBox();
+            //设置个人水平盒子
+            hBox.setPrefSize(300, 240);
+            //设置内边距
+            hBox.setSpacing(25);
+            hBox.setPadding(new Insets(20, 20, 20, 20));
+            //美化水平盒子  调用css中的box
+            hBox.getStyleClass().add("box");
+            //创建左侧的垂直布局
+            VBox leftBox = new VBox();
+            leftBox.setSpacing(10);
+            //对齐方式
+            leftBox.setAlignment(Pos.TOP_CENTER);
+            //头像图片  如果本地文件则url:"/img?
+            ImageView pictureImg = new ImageView(new Image(entity.getStr("picture")));
+            pictureImg.setFitWidth(100);
+            pictureImg.setFitHeight(100);
+            //给头像设置圆形效果
+            Circle circle = new Circle();
+            circle.setCenterX(40);
+            circle.setCenterY(40);
+            circle.setRadius(40);
+            pictureImg.setClip(circle);
+            //头像加入左边盒子
+            leftBox.getChildren().add(pictureImg);
+            hBox.getChildren().add(leftBox);
+            //创建右边垂直布局盒子
+            VBox rightBox = new VBox();
+            rightBox.setSpacing(10);
+            rightBox.setAlignment(Pos.TOP_CENTER);
+            Label namelabel = new Label(entity.getStr("name"));//会员名字
+            Label yearlabel = new Label(entity.getStr("year"));//会员年份
+            Label jifenlabel = new Label(entity.getStr("jifen"));//会员积分
+            Label mobilelabel = new Label(entity.getStr("mobile"));//会员电话
+            Label addresslabel = new Label(entity.getStr("address"));//会员地址
+            Button button = new Button("删除");
+            button.getStyleClass().add("menu1-btn");
+            //点击删除按钮要做的事情
+            button.setOnAction(event -> {
+                //1弹出一个确认的对话框
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setTitle("确认对话框");
+                alert.setContentText("确定删除该会员信息吗？");
+                //2点击了“确认”按钮
+                Optional<ButtonType> result = alert.showAndWait();
+                if (result.get() == ButtonType.OK) {
+                    //得到个人的id
+                    long id = entity.getLong("id");
+                    try {
+                        //调用readerDAO的删除方法
+                        vipDAO.deleteById(id);
+                        //从流式面板里面删除掉
+                        vipPane.getChildren().remove(hBox);
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            rightBox.getChildren().addAll(namelabel,yearlabel,jifenlabel,mobilelabel,addresslabel,button);
+            hBox.getChildren().add(rightBox);
+            vipPane.getChildren().add(hBox);
+        }
+    }
+    public void addVip() throws SQLException {
+        Vip vip = new Vip();
+        //新建一个舞台
+        Stage stage = new Stage();
+        stage.setTitle("新增会员界面");
+        //创建一个垂直布局，用来放新增用户的各个组件
+        VBox vBox = new VBox();
+        vBox.setSpacing(10);
+        vBox.setPadding(new Insets(20, 10, 10, 10));
+        //创建一个日期选择器对象，并初始化值为当前日期
+        DatePicker datePicker = new DatePicker();
+        datePicker.setValue(LocalDate.now());
+        TextField nameField = new TextField("请输入姓名");
+        TextField pictureField = new TextField("请输入头像地址");
+        TextField addressField = new TextField("请输入会员地址");
+        TextField jifenField = new TextField("新增会员的积分");
+        TextField yearField = new TextField("新增会员的年限");
+        TextField mobileField = new TextField("请输入手机号码");
+        Button addBtn = new Button("新增");
+        addBtn.getStyleClass().add("brown-theme");
+        vBox.getChildren().addAll(nameField,pictureField,datePicker,jifenField,yearField,addressField,mobileField,addBtn);
+        Scene scene = new Scene(vBox, 540, 450);
+        stage.setScene(scene);
+        stage.show();
+        //点击新增按钮，将界面数据封装成一个Reader对象，写入数据库
+        addBtn.setOnAction(event -> {
+            String nameString = nameField.getText().trim();
+            String pictureString = pictureField.getText().trim();
+            String addressString = addressField.getText().trim();
+            String jifenString = jifenField.getText().trim();
+            String yearString = yearField.getText().trim();
+            String mobileString = mobileField.getText().trim();
+            vip.setAddress(addressString);
+            vip.setMobile(mobileString);
+            vip.setName(nameString);
+            vip.setPicture(pictureString);
+            vip.setJifen(jifenString);
+            vip.setYear(yearString);
+            System.out.println(vip.getName() + vip.getAddress() + vip.getPicture()+vip.getJifen()+vip.getYear());
+            try {
+                vipDAO.insertVip(vip);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            stage.close();
+            try {
+                vipList = vipDAO.selectVip();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            showVip(vipList);
+        });
+    }
+}
---
+++ src/main/java/com/soft1841/sm/entity/Vip.java
@@ -0,0 +1,79 @@
+package com.soft1841.sm.entity;
+
+public class Vip {
+    private String id;
+    private String name;
+    private String year;
+    private String picture;
+    private String jifen;
+    private String mobile;
+    private String address;
+
+    public Vip(String id, String name, String year, String picture, String jifen, String mobile, String address) {
+        this.id = id;
+        this.name = name;
+        this.year = year;
+        this.picture = picture;
+        this.jifen = jifen;
+        this.mobile = mobile;
+        this.address = address;
+    }
+    public Vip() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getJifen() {
+        return jifen;
+    }
+
+    public void setJifen(String jifen) {
+        this.jifen = jifen;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
